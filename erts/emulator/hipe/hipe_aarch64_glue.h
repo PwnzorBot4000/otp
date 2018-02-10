3a566fd43068e55953d7c251f06706dc3615f0b2
Kostis Sagonas <kostis@it.uu.se>
2018-02-10 18:41:12 +0100

    Rename the HIPE_ARCH functions in hipe/hipe_aarch64_glue.S

--- erts/emulator/hipe/hipe_aarch64_glue.h
+++ erts/emulator/hipe/hipe_aarch64_glue.h
@@ -5,10 +5,10 @@
 
 #include "hipe_aarch64_asm.h"		/* for NR_ARG_REGS */
 #define NR_LEAF_WORDS			AARCH64_LEAF_WORDS
-// #define HIPE_ARCH_CALL_TO_NATIVE	hipe_arm_call_to_native
-// #define HIPE_ARCH_RETURN_TO_NATIVE	hipe_arm_return_to_native
-// #define HIPE_ARCH_TAILCALL_TO_NATIVE	hipe_arm_tailcall_to_native
-// #define HIPE_ARCH_THROW_TO_NATIVE	hipe_arm_throw_to_native
+#define HIPE_ARCH_CALL_TO_NATIVE	hipe_aarch64_call_to_native
+#define HIPE_ARCH_RETURN_TO_NATIVE	hipe_aarch64_return_to_native
+#define HIPE_ARCH_TAILCALL_TO_NATIVE	hipe_aarch64_tailcall_to_native
+#define HIPE_ARCH_THROW_TO_NATIVE	hipe_aarch64_throw_to_native
 #include "hipe_risc_glue.h"
 
 #endif /* HIPE_AARCH64_GLUE_H */
