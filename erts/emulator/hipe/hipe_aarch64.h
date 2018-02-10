25cb2156ce87c3ca2426d41ca6f78ecea82b1130
Kostis Sagonas <kostis@it.uu.se>
2018-02-10 18:41:42 +0100

    Add a missing function

--- erts/emulator/hipe/hipe_aarch64.h
+++ erts/emulator/hipe/hipe_aarch64.h
@@ -9,10 +9,16 @@ extern void hipe_flush_icache_range(void *address, unsigned int nbytes);
 /* for stack descriptor hash lookup */
 #define HIPE_RA_LSR_COUNT	3	/* low 3 bits are always zero */
 
-/* for hipe_bifs_{read,write}_{s,u}32 */
+/* for hipe_bifs_{read,write}_{s,u}32 -- XXX: most likely, NOT needed */
 static __inline__ int hipe_word32_address_ok(void *address)
 {
     return ((unsigned long)address & 0x3) == 0;
+}
+
+/* for hipe_bifs_{read,write}_{s,u}64 */
+static __inline__ int hipe_word64_address_ok(void *address)
+{
+    return 1;
 }
 
 /* Native stack growth direction. */
