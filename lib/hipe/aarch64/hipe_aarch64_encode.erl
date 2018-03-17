f67c8cd97bc41f5910a8a546a755368137ef74ea
Thanasis Papoutsidakis <pwnzorbot4000@gmail.com>
2018-03-17 16:32:43 +0000

    Add 'ldr' encoding
    Immediate offset & pc-relative literal

--- lib/hipe/aarch64/hipe_aarch64_encode.erl
+++ lib/hipe/aarch64/hipe_aarch64_encode.erl
@@ -68,6 +68,17 @@ sub(Opnds) -> data_addsub_form(2#1, Opnds).
 ldstr_imm_form(Size, V, Opc, Imm12, Rn, Rt) ->
   ?BF(31,30,Size) bor ?BF(29,27,2#111) bor ?BIT(26,V) bor ?BF(25,24,2#01) bor ?BF(23,22,Opc) bor ?BF(21,10,Imm12) bor ?BF(9,5,Rn) bor ?BF(4,0,Rt).
 
+ldstr_pcrel_form(Opc, V, Imm19, Rt) ->
+  ?BF(31,30,Opc) bor ?BF(29,27,2#011) bor ?BIT(26,V) bor ?BF(25,24,2#00) bor ?BF(23,5,Imm19) bor ?BF(4,0,Rt).
+
+ldr({_Cond, {r, Dst}, Src}) ->
+  case Src of
+    {'immediate_offset', {r, Base}, {imm12, Offset}} ->
+      ldstr_imm_form(2#11, 2#0, 2#01, Offset, Base, Dst);
+    {'pc-relative', {imm19, Offset}} ->
+      ldstr_pcrel_form(2#01, 2#0, Offset, Dst)
+  end.
+
 str({_Cond, {r, Src}, Dst}) ->
   case Dst of
     {'immediate_offset', {r, Base}, {imm12, Offset}} ->
@@ -80,6 +91,7 @@ str({_Cond, {r, Src}, Dst}) ->
 
 insn_encode(Op, Opnds) ->
   case Op of
+    'ldr' -> ldr(Opnds);
     'str' -> str(Opnds);
     'sub' -> sub(Opnds);
     _ -> exit({?MODULE,insn_encode,Op})
